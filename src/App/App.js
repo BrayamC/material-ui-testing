ca3174e807da8f6adedae75c4acdb8586e0f07f5
Brayam Corral Flores <brayamcorral@gmail.com>
2021-11-15 13:41:58 -0800

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SideMenu from "../components/SideMenu"
+
+function App() {
+  return (
+    <React.Fragment>
+      <SideMenu />
+      <div> Test </div>
+   </React.Fragment>
+  );
+}
+
+export default App;
